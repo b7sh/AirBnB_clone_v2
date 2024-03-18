c6f31eb8e814ae368aee38de97225bd0a8fe5a86
b7sh <boshy@DESKTOP-0N80D6I.localdomain>
2024-03-19 01:05:10 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-import models
 from datetime import datetime
 
 
@@ -30,9 +29,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
