5e164faf7a162dbb6450edf53afcf61b5e3de910
b7sh <boshy@DESKTOP-0N80D6I.localdomain>
2024-03-21 01:58:54 +0300

    p

--- models/user.py
+++ models/user.py
@@ -18,4 +18,3 @@ class User(BaseModel, Base):
                                backref="user")
     reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="user")
-    
