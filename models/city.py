22a8b4ed5ecdf14bbeb1030cd07c36c0f26ee910
b7sh <boshy@DESKTOP-0N80D6I.localdomain>
2024-03-21 00:42:54 +0300

    update

--- models/city.py
+++ models/city.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel, Base):
     """ 
@@ -12,3 +14,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                               backref="place")
--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from models.place import Place
 from models.review import Review
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,3 +14,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                               backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="user")
+    
