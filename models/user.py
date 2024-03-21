bf1f4c1652217cab1573134ca3ead81d1adcc0f8
b7sh <boshy@DESKTOP-0N80D6I.localdomain>
2024-03-21 04:43:33 +0300

    s

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ from models.review import Review
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
+class User(Base, BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
