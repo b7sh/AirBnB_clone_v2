0b38b0667547c786b5f5b270450a3869c797ab71
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-22 00:08:48 -0700

    m

--- models/place.py
+++ models/place.py
@@ -21,3 +21,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                backref="place")
--- models/review.py
+++ models/review.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForienKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    place_id = column(String(128), ForienKey("places.id"), nullable=False)
+    user_id = column(String(128), ForienKey("users.id"), nullable=False)
+    text = column(String(128), nullable=False)
