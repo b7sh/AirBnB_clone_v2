3e634581cd097817d4169788fdc516d4685f40b3
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-22 00:14:12 -0700

    changes

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForienKey
+from sqlalchemy import String, Column, ForeignKey
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    place_id = column(String(128), ForienKey("places.id"), nullable=False)
-    user_id = column(String(128), ForienKey("users.id"), nullable=False)
-    text = column(String(128), nullable=False)
+    place_id = Column(String(128), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(128),ForeignKey("users.id"), nullable=False)
+    text = Column(String(128), nullable=False)
