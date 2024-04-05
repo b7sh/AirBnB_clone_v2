ce86feaa681f4df74d38c5c3b7323f755ba66dee
b7sh <mohammedabdelbasit500@gmail.com>
2024-04-04 19:39:58 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,20 +13,17 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     """ distributes an archive to your web servers """
     if os.path.exists(archive_path):
-        try:
-            path = archive_path.split('/')[-1]
-            c_path = "/tmp/{}".format(path)
-            fol = path.split('.')[0]
-            fol_path = "/data/web_static/releases/"
+        path = archive_path.split('/')[-1]
+        fol = path.split('.')[0]
+        fol_path = "/data/web_static/releases/"
 
-            put(archive_path, '/tmp/')
-            run("mkdir -p {}{}/".format(fol_path, fol))
-            run("tar -xzf /tmp/{} -C {}{}/".format(path, fol_path, fol))
-            run("rm /tmp/{}".format(path))
-            run("mv {0}{1}/web_static/* {0}{1}/".format(fol_path, fol))
-            run("rm -rf {}{}/web_static".format(fol_path, fol))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {}{}/ /data/web_static/current".format(fol_path, fol))
-            return True
-        except:
-            return False
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}{}/".format(fol_path, fol))
+        run("tar -xzf /tmp/{} -C {}{}/".format(path, fol_path, fol))
+        run("rm /tmp/{}".format(path))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(fol_path, fol))
+        run("rm -rf {}{}/web_static".format(fol_path, fol))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(fol_path, fol))
+        return True
+    return False
