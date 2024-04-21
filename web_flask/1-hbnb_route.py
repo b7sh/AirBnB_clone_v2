5be45d6a19301c37f83d2e1bb52a3ec1a9324f9d
b7sh <mohammedabdelbasit500@gmail.com>
2024-04-21 09:39:58 +0300

    update_2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def index():
     ''' Return Hello HBNB! '''
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     ''' Return HBNB '''
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
